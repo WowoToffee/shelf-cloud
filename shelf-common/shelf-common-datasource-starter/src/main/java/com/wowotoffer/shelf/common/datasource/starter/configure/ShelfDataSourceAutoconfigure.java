662411e9980514d480d1972c680bd942a3116ea3
oufan <1581743312@qq.com>
2021-04-20 11:18:06 +0800

    update 完善db 公共接口

--- shelf-common/shelf-common-datasource-starter/src/main/java/com/wowotoffer/shelf/common/datasource/starter/configure/ShelfDataSourceAutoconfigure.java
+++ shelf-common/shelf-common-datasource-starter/src/main/java/com/wowotoffer/shelf/common/datasource/starter/configure/ShelfDataSourceAutoconfigure.java
@@ -1,9 +1,42 @@
 package com.wowotoffer.shelf.common.datasource.starter.configure;
 
+import com.baomidou.mybatisplus.core.parser.ISqlParser;
+import com.baomidou.mybatisplus.extension.parsers.BlockAttackSqlParser;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.wowotoffer.shelf.common.datasource.starter.interceptor.DataPermissionInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author of
  * @version 1.0
  * @date 2021/4/19 21:31
  */
+@Configuration
 public class ShelfDataSourceAutoconfigure {
+    /**
+     * 注册数据权限
+     */
+    @Bean
+    @Order(-1)
+    public DataPermissionInterceptor dataPermissionInterceptor() {
+        return new DataPermissionInterceptor();
+    }
+
+    /**
+     * 注册分页插件
+     */
+    @Bean
+    @Order(-2)
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        List<ISqlParser> sqlParserList = new ArrayList<>();
+        sqlParserList.add(new BlockAttackSqlParser());
+        paginationInterceptor.setSqlParserList(sqlParserList);
+        return paginationInterceptor;
+    }
 }
